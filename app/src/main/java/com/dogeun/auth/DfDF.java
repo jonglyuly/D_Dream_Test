31c34860e9032e9bf10b3f471d144cfb46e2126b
LEEDOGEUN <jonglyuly@naver.com>
2019-03-12 22:27:08 +0900

    .

---
+++ app/src/main/java/com/dogeun/auth/DfDF.java
@@ -0,0 +1,12 @@
+package com.dogeun.auth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DfDF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
